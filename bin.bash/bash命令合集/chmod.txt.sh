fc0ae3c6225f377ff8c7ebb1b5392827995ee68e
ikwen <ikwen@163.com>
2020-09-04 18:47:09 +0800

    delete2

---
+++ bin.bash/bash命令合集/chmod.txt.sh
@@ -0,0 +1,7 @@
+https://zhidao.baidu.com/question/328388595.html
+
+加入-R 参数，就可以将读写权限传递给子文件夹zhidao
+例如chmod -R 777 /home/mypackage
+那么mypackage 文件夹和它下面的所有子文件夹的属性都变成了专777.
+777是读、写、执行权限...
+我也就属知道这么多啦...希望能帮到你~
