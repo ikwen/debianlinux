e6b49cbe9ff44da9cb0d70190aaedc89af4ece24
ikwen <ikwen@163.com>
2021-06-01 03:59:54 +0800

    linux

--- u盘linux/efi.sh
+++ u盘linux/efi.sh
@@ -7,3 +7,16 @@ mount -o bind /sys /mnt/sys&&
 mount -o bind /run /mnt/run&&
 chroot /mnt&&
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=debian
+
+
+
+
+
+
+1\ UEFI system and gpt format disk
+2\root@debian:/# grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=debian
+Installing for x86_64-efi platform.
+grub-install: warning: EFI variables are not supported on this system..
+Installation finished. No error reported.
+
+right!!!
