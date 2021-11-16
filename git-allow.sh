60710a15658a995d7a6b0e9fe03a44815e77ff4d
ikwen <ikwen@163.com>
2021-11-16 03:56:29 -0500

    linux

--- git-allow.sh
+++ git-allow.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git pull origin master --allow-unrelated-histories&&git&& add .&&git commit -m "zhengji"&&git push origin master&&echo "hello,ikwen,github is update OKOKOK"
+git pull origin master --allow-unrelated-histories&&git add .&&git commit -m "zhengji"&&git push origin master&&echo "hello,ikwen,github is update OKOKOK"
